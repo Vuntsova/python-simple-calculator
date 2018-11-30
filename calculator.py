50f4de1d8603c7375366b45d43f14a214b2d09b2
Emiliya <vuntsova@aol.com>
2018-11-30 13:42:01 -0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input('Enter first number: '))
+op = input('Enter operator(- + / *): ')
+num2 = float(input('Enter second number: '))
+
+if op == '+':
+    results = num1 + num2
+elif op == '-':
+    results = num1 - num2
+elif op == '/':
+    results = num1 / num2
+else:
+    results = num1 * num2
+
+print(results)
